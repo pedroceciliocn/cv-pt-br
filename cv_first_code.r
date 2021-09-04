3b2abaa29e6bce0cb2bb5b60ce9e323b73da95d7
Pedro Neto <pedroceciliocn@gmail.com>
2021-09-04 15:01:24 -0300

    cv pt-br v1

---
+++ cv_first_code.r
@@ -0,0 +1,9 @@
+datadrivencv::use_datadriven_cv(
+  full_name = "Pedro Neto",
+  data_location = "https://docs.google.com/spreadsheets/d/1dqI1pkJdKFqjC4IR2vg8sKzbJZjdn7b1Rn4Ohtt61GY/edit?usp=sharing",
+  pdf_location = "https://github.com/pedroceciliocn/cv-pt-br/cv-pt.pdf",
+  html_location = "https://pedroccneto.netlify.app/media/cv-pt.html",
+  source_location = "https://github.com/pedroceciliocn/cv-pt-br",
+  use_network_logo = TRUE,
+  open_files = TRUE
+)
